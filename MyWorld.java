ba923ecc5a2b98ac5ad81f0550c4634768c86a89
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-19 12:59:34 -0400

    Add Cherry Object

--- MyWorld.java
+++ MyWorld.java
@@ -6,6 +6,9 @@ public class MyWorld extends World
     {    
         super(600, 400, 1);
         Bear bear = new Bear();
-        addObject(bear, 300, 200);
+        addObject(bear, 300, 300);
+        
+        Cherry cherry = new Cherry();
+        addObject(cherry, 300, 100);
     }
 }
