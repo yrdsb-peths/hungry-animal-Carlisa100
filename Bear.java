33be12cb0d3fab657f9b2ff68e069ba01adbe33b
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-24 23:26:13 -0400

    Elephant Animation Code

--- Bear.java
+++ Bear.java
@@ -2,6 +2,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Bear extends Actor
 {
+<<<<<<< Updated upstream
+=======
+    GreenfootSound bearSound = new GreenfootSound("bear-roar.mp3"); 
+    GreenfootImage[] idle = new GreenfootImage[8]; 
+    
+    public void Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("Downloads/Elephant Sprite/tile000" + i + ".png"); 
+        }
+        setImage(idle[0]); 
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = imageIndex + 1 % idle.length; 
+    }
+    
+>>>>>>> Stashed changes
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
@@ -12,6 +34,10 @@ public class Bear extends Actor
         {
             move(2);
         }
+        
+        eat();
+        
+        animateElephant(); 
     }
     
     public void eat()
